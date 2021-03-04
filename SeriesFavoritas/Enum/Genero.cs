eff834b4510367fa475ed163b97b7191cf472b7e
mkeller <marcoskeller@hotmail.com>
2021-03-04 16:23:58 -0300

    Correcao do Enum

--- SeriesFavoritas/Enum/Genero.cs
+++ SeriesFavoritas/Enum/Genero.cs
@@ -19,24 +19,4 @@ namespace SeriesFavoritas.Enum
 		Suspense = 12,
 		Terror = 13
 	}
-
-	public enum GetValuesTest
-	{
-		
-			Acao = 1,
-			Aventura = 2,
-			Comedia = 3,
-			Documentario = 4,
-			Drama = 5,
-			Espionagem = 6,
-			Faroeste = 7,
-			Fantasia = 8,
-			Ficcao_Cientifica = 9,
-			Musical = 10,
-			Romance = 11,
-			Suspense = 12,
-			Terror = 13
-		
-	}
-
 }
